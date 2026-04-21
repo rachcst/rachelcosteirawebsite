5906e7ff9615aaa1b6c597458a0ace899f80ed66
rachcst <rachelcosteira@gmail.com>
2026-04-21 10:19:34 +0100

    fixing deploy issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://rachcst.github.io',
-  base: 'rachelcosteirawebsite',
+  base: '/rachelcosteirawebsite',
 })
