9306db2e0fe0590aed38eb17ee6169c5446205fa
rachcst <rachelcosteira@gmail.com>
2026-04-23 21:30:26 +0100

    added search feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: 'https://rachelcosteira.com',
   base: '/',
+  integrations: [pagefind()],
 });
+
